ed3c5029f137e6fe06f4c95b5edf88f665bd9017
spences10 <spences10apps@gmail.com>
2021-12-15 16:54:39 +0000

    feat: :sparkles: add sitemap endpoint
    add robots.txt

---
+++ src/routes/sitemap.xml.js
@@ -0,0 +1,77 @@
+import { client } from '$lib/graphql-client'
+import { gql } from 'graphql-request'
+
+const website = 'https://www.myporfolioproject.com'
+
+export const get = async () => {
+  const query = gql`
+    query Posts {
+      posts {
+        title
+        slug
+        date
+        content
+        tags
+        coverImage {
+          url
+        }
+        authors {
+          name
+        }
+      }
+    }
+  `
+  const { posts } = await client.request(query)
+  const pages = [`about`]
+  const body = sitemap(posts, pages)
+
+  const headers = {
+    'Cache-Control': 'max-age=0, s-maxage=3600',
+    'Content-Type': 'application/xml',
+  }
+  return {
+    headers,
+    body,
+  }
+}
+
+const sitemap = (
+  posts,
+  pages
+) => `<?xml version="1.0" encoding="UTF-8" ?>
+<urlset
+  xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:xhtml="https://www.w3.org/1999/xhtml"
+  xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+>
+  <url>
+    <loc>${website}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.7</priority>
+  </url>
+  ${pages
+    .map(
+      page => `
+  <url>
+    <loc>${website}/${page}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.7</priority>
+  </url>
+  `
+    )
+    .join('')}
+  ${posts
+    .map(
+      post => `
+  <url>
+    <loc>${website}/posts/${post.slug}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.7</priority>
+  </url>
+  `
+    )
+    .join('')}
+</urlset>`
