ce1eba9b93e830ba18e773c341d69273f1d1d8d5
spences10 <spences10apps@gmail.com>
2021-12-21 20:11:52 +0000

    refactor: :recycle: remove index from ProjectCard
    not needed anymore

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -47,13 +47,7 @@
 <div
   class="grid gap-10 md:grid-cols-4 md:px-10 lg:grid-cols-6 lg:-mx-52"
 >
-  {#each projects as { name, slug, description, image }, index}
-    <ProjectCard
-      {name}
-      {description}
-      url={image[0].url}
-      {index}
-      {slug}
-    />
+  {#each projects as { name, slug, description, image }}
+    <ProjectCard {name} {description} url={image[0].url} {slug} />
   {/each}
 </div>
