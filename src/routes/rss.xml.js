bd0650e631335506c51dcb018d80e56cf2a7f30c
spences10 <spences10apps@gmail.com>
2021-12-21 17:00:36 +0000

    feat: :sparkles: add RSS feed in endpoint

---
+++ src/routes/rss.xml.js
@@ -0,0 +1,58 @@
+import { client } from '$lib/graphql-client'
+import { gql } from 'graphql-request'
+
+const name = 'My Portfolio'
+const website = 'https://myportfolio.com'
+
+export const get = async () => {
+  const query = gql`
+    query Posts {
+      posts {
+        title
+        slug
+      }
+    }
+  `
+  const { posts } = await client.request(query)
+  const body = xml(posts)
+
+  const headers = {
+    'Cache-Control': 'max-age=0, s-maxage=3600',
+    'Content-Type': 'application/xml',
+  }
+  return {
+    headers,
+    body,
+  }
+}
+
+const xml =
+  posts => `<rss xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom" version="2.0">
+  <channel>
+    <title>${name}</title>
+    <link>${website}</link>
+    <description>This is my portfolio!</description>
+    ${posts
+      .map(
+        post =>
+          `
+        <item>
+          <title>${post.title}</title>
+          <description>This is my portfolio!</description>
+          <link>${website}/posts/${post.slug}/</link>
+          <pubDate>${new Date(post.date)}</pubDate>
+          <content:encoded>${post.previewHtml} 
+            <div style="margin-top: 50px; font-style: italic;">
+              <strong>
+                <a href="${website}/posts/${post.slug}">
+                  Keep reading
+                </a>
+              </strong>  
+            </div>
+          </content:encoded>
+        </item>
+      `
+      )
+      .join('')}
+  </channel>
+</rss>`
