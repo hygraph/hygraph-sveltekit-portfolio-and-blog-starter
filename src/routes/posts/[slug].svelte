a412033611723a98390b2d2a16d1576822749d1b
spences10 <spences10apps@gmail.com>
2021-12-21 17:30:39 +0000

    style: :lipstick: rounded on hero image
    keep it consistent

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -28,9 +28,9 @@
 
 <div class="sm:-mx-5 md:-mx-10 lg:-mx-20 xl:-mx-38 mb-5">
   <img
+    class="rounded-xl"
     src={coverImage.url}
     alt={`Cover image for ${title}`}
-    class=""
   />
 </div>
 
