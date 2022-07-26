51081abbd8ea7cac6b1cb36634361397b5ccfe7c
spences10 <spences10apps@gmail.com>
2022-07-26 10:04:44 +0100

    refactor: :recycle: update SvelteKit to use Vite config
    updated the app.html to use the new SvelteKit variables

--- svelte.config.js
+++ svelte.config.js
@@ -6,14 +6,11 @@ import preprocess from 'svelte-preprocess'
 const config = {
   kit: {
     adapter: adapter(),
-    vite: {
-      resolve: {
-        alias: {
-          $components: path.resolve('./src/lib/components'),
-          $lib: path.resolve('./src/lib'),
-          $stores: path.resolve('./src/stores'),
-        },
-      },
+
+    alias: {
+      $components: path.resolve('./src/lib/components'),
+      $lib: path.resolve('./src/lib'),
+      $stores: path.resolve('./src/stores'),
     },
   },
 
