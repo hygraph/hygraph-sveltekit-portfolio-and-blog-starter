624fc966eb97e11c6c4d6d4c84f4704a042a92a0
Martian Lee <martian2lee@163.com>
2022-09-03 01:42:14 +0800

    refactor: unify name of starter variables for Vercel deployments (#10)

--- src/lib/graphql-client.js
+++ src/lib/graphql-client.js
@@ -1,4 +1,4 @@
 import { GraphQLClient } from 'graphql-request'
-const GRAPHQL_ENDPOINT = import.meta.env.VITE_GRAPHQL_API
+const HYGRAPH_ENDPOINT = import.meta.env.HYGRAPH_ENDPOINT
 
-export const client = new GraphQLClient(GRAPHQL_ENDPOINT)
+export const client = new GraphQLClient(HYGRAPH_ENDPOINT)
