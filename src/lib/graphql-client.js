800759022b6162fc1177b883712516006013d282
João Pedro Schmitz <oi@joaopedro.cc>
2022-10-28 13:29:00 -0300

    Update getting started guide and change env variable name (#14)

--- src/lib/graphql-client.js
+++ src/lib/graphql-client.js
@@ -1,4 +1,4 @@
 import { GraphQLClient } from 'graphql-request'
-const HYGRAPH_ENDPOINT = import.meta.env.HYGRAPH_ENDPOINT
+const HYGRAPH_ENDPOINT = import.meta.env.VITE_HYGRAPH_ENDPOINT
 
 export const client = new GraphQLClient(HYGRAPH_ENDPOINT)
